93252c2daba8fa5d6015349885217725c791d4a9
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-07 21:37:09 +0700

    tambahkan judul "Formulir Pendaftaran" pada header Form

--- app/src/main/java/com/example/questnavigastugas_147/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_147/view/Formulir.kt
@@ -1,28 +1,11 @@
 package com.example.questnavigastugas_147.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -75,7 +58,17 @@ fun Form(modifier: Modifier = Modifier,
                         )
                     )
                     .padding(vertical = 24.dp)
-            )
+            ) {
+                Text(
+                    text = "Formulir Pendaftaran",
+                    color = Color.White,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+            }
         }
     }
 }
