dbbf7e58cdc95f5edac154e51bafa25a759a6969
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-07 21:45:59 +0700

    tambah input field untuk alamat pada form pendaftaran

--- app/src/main/java/com/example/questnavigastugas_147/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_147/view/Formulir.kt
@@ -1,6 +1,7 @@
 package com.example.questnavigastugas_147.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,12 +10,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -143,6 +149,18 @@ fun Form(modifier: Modifier = Modifier,
                             Text(text = item, modifier = Modifier.padding(start = 8.dp))
                         }
                     }
+
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = "ALAMAT", fontWeight = FontWeight.Bold)
+                    OutlinedTextField(
+                        value = textAlamat,
+                        onValueChange = { textAlamat= it },
+                        label = { Text("Isian Alamat") },
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                    )
                 }
             }
         }
