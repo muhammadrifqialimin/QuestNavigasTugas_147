2b82c1252ca3c1a871b48945d60ce42767223ccd
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-11-07 21:34:24 +0700

    tambahkan Box dengan background gradasi vertikal pada tampilan Form

--- app/src/main/java/com/example/questnavigastugas_147/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas_147/view/Formulir.kt
@@ -1,12 +1,42 @@
 package com.example.questnavigastugas_147.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Form(modifier: Modifier = Modifier,
@@ -20,4 +50,16 @@ fun Form(modifier: Modifier = Modifier,
 
     val gender = listOf("Laki-laki", "Perempuan")
     val status = listOf("Janda", "Lajang", "Duda")
+
+    Box(
+        modifier = Modifier
+            .padding(top = 30.dp)
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(Color(0xFFE91E63), Color.Red)
+                )
+            ),
+        contentAlignment = Alignment.TopCenter
+    )
 }
